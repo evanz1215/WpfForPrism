52c73da13fcbff046908f48850e62b92353659e1
Evan <evanz1215@gmail.com>
2024-06-08 15:48:34 +0800

    簡易UC切換

--- WpfForPrism/App.xaml.cs
+++ WpfForPrism/App.xaml.cs
@@ -1,12 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
 using System.Windows;
+using WpfForPrism.Views;
 
 namespace WpfForPrism;
+
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : PrismApplication
 {
-}
+    /// <summary>
+    /// 設置啟動頁
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
 
+    /// <summary>
+    /// 注入服務
+    /// </summary>
+    /// <param name="containerRegistry"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+}
---
+++ WpfForPrism/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfForPrism.Views;
+
+namespace WpfForPrism.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand<string> ShowContentCmm { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ShowContentCmm = new DelegateCommand<string>(ShowContentFunc);
+        }
+
+        /// <summary>
+        /// 改變顯示用戶控件
+        /// </summary>
+        /// <param name="viewName"></param>
+        private void ShowContentFunc(string viewName)
+        {
+            if (viewName == "UCA")
+            {
+                this.ShowContent = new UCA();
+            }
+            else if (viewName == "UCB")
+            {
+                this.ShowContent = new UCB();
+            }
+            else
+            {
+                this.ShowContent = new UCC();
+            }
+        }
+
+        /// <summary>
+        /// 顯示的內容
+        /// </summary>
+        private UserControl _showContent;
+
+        /// <summary>
+        /// 顯示的內容
+        /// </summary>
+        public UserControl ShowContent
+        {
+            get { return _showContent; }
+            set
+            {
+                _showContent = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+    }
+}
